efb97513d4d87ba1cba2a854790469b7b5c6b470
TBN2 <lucashaytink@gmail.com>
2020-10-12 21:08:07 +0200

    De invoervelden verplicht gemaakt,
    Invoervelden verplicht gemaakt, zodat de formulieren niet zonder enige inhgoud worden verzonden

--- AddBlog.php
+++ AddBlog.php
@@ -6,11 +6,11 @@
         <title>Add new post</title>
     </head>
     <body>
-    	<main id="JelleGay">
+    	<main class="addblog">
 
 	        <form id="mylittleformy">
-	        	<select name="Naam Docent" id="Ndocent">
-	        		<option value="" disabled selected>Select Catagory *</option>
+	        	<select name="Category" required id="ABcategory">
+	        		<option value="" disabled selected>Select Category *</option>
 	        		<option value="Gerjan">1</option>
 	        		<option value="Winnie">2</option>
 	        		<option value="Rene">3</option>
@@ -21,9 +21,9 @@
 	        		<option value="Jan">8</option>
 	        	</select><br>
 	        	<label class="l1" for="NameArticle">Title: *</label><br>
-	        	<input type="text" id="NameArticle" placeholder="Input Title"><br>
+	        	<input type="text" required id="NameArticle" placeholder="Input Title"><br>
 	        	<label class="l2" for= "AddBlog">Description *</label><br>
-	        	<textarea class="AddBlog" id="AddBlog" placeholder="Input text..."></textarea>
+	        	<textarea class="AddBlog" required id="AddBlog" placeholder="Input text..."></textarea>
 
 	        	<div class="butt">
 	        		<button class="white" type="reset">Reset</button>
