37fe37724066e1bdbd4dfdad9511ad85b5b8b39e
TBN2 <lucashaytink@gmail.com>
2020-10-12 18:03:58 +0200

    Kleur en buttons aangepast

--- AddBlog.php
+++ AddBlog.php
@@ -6,31 +6,29 @@
         <title>Add new post</title>
     </head>
     <body>
-    	<main id="AddBlog">
+    	<main id="JelleGay">
 
 	        <form id="mylittleformy">
 	        	<select name="Naam Docent" id="Ndocent">
-	        		<option value=""disabled selected>Select Catagory *</option>
-	        		<option value="Gerjan"></option>
-	        		<option value="Winnie"></option>
-	        		<option value="Rene"></option>
-	        		<option value="Raymond"></option>
-	        		<option value="Albert"></option>
-	        		<option value="RobL"></option>
-	        		<option value="Robs"></option>
-	        		<option value="Jan"></option>
+	        		<option value="" disabled selected>Select Catagory *</option>
+	        		<option value="Gerjan">1</option>
+	        		<option value="Winnie">2</option>
+	        		<option value="Rene">3</option>
+	        		<option value="Raymond">4</option>
+	        		<option value="Albert">5</option>
+	        		<option value="RobL">6</option>
+	        		<option value="Robs">7</option>
+	        		<option value="Jan">8</option>
 	        	</select><br>
-	        	<label for="name article">Title: *</label><br>
-	        	<input type="text" name="name article" placeholder="Input Title"><br>
-	        	<label for= "AddBlog">Description *</label><br>
-	        	<textarea class="AddBlog" placeholder="Input text..."></textarea>
+	        	<label class="l1" for="NameArticle">Title: *</label><br>
+	        	<input type="text" id="NameArticle" placeholder="Input Title"><br>
+	        	<label class="l2" for= "AddBlog">Description *</label><br>
+	        	<textarea class="AddBlog" id="AddBlog" placeholder="Input text..."></textarea>
 
-	        	<button type="submit">Submit</button>
-	        	<button type="reset">Reset</button>
-	        	<?php
-	        	
-
-	        	?>
+	        	<div class="butt">
+	        		<button class="white" type="reset">Reset</button>
+	        		<button class="blue" type="submit">Add</button>
+	        	</div>
 	    	</form>
     	</main>
     </body>
