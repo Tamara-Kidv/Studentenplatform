c72daef5325e04ccbdc1f8811bb9aff109d916f6
KillerSlang <72264057+KillerSlang@users.noreply.github.com>
2020-10-12 21:40:56 +0200

    Home beginsel
    main aangemaakt, H2 & H3 met wat test <p>'s en in css de bijbehorende grids aangepast

--- home.php
+++ home.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <link rel="stylesheet" href="style.css">
         <meta charset="UTF-8">
         <title>Home</title>
     </head>
     <body>
+        <main>
         <h1>Home</h1>
+            <div id="Lgrid">
+                <h2 id="homekopje">Latest News</h2>
+                <h3 class="onderkopje">yesss??</h3>
+                <p> work with me goddammit, DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT DEUS VULT </p>
+                <h4 class="onderkopje">YEET</h4>
+                <p> Союз нерушимый республик свободных
+Сплотила навеки Великая Русь
+Да здравствует созданный волей народов
+Единый, могучий Советский Союз
+Славься, Отечество наше свободное
+Дружбы народов надёжный оплот!
+Партия Ленина - сила народная
+Нас к торжеству коммунизма ведёт
+Сквозь грозы сияло нам солнце свободы
+И Ленин великий нам путь озарил
+На правое дело он поднял народы
+На труд и на подвиги нас вдохновил
+Славься, Отечество наше свободное
+Дружбы народов надёжный оплот
+Партия Ленина - сила народная
+Нас к торжеству коммунизма ведёт</p>
+            </div>
+        <hr>
+        <div id="Mgrid">
+            
+        </div>
+        </main>
         <?php
         
         ?>
