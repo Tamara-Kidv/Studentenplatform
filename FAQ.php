08696d58a3f8e7d1f1f17b5942399c293f689001
Tamara <tamarakijkindevegte@live.nl>
2020-10-19 12:22:54 +0200

    Changed template.php
    Fixed main template so user can switch between the pages.
    Added FAQ page.

--- FAQ.php
+++ FAQ.php
@@ -1,60 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>FAQ</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
-    <body>
-    	<header>
-    		<h1>FAQ</h1>
-    	</header>
-    	<main id="faqMain">
-    		<section id="faqUserSearchBox">	        
-    			<form>
-    				<button type="submit" class="searchBarButton"><li class="fa fa-search iconSearch"></li></button>
-		        	<input type="text" name="search" class="searchBar" placeholder="Search...">
-		        	
-		        </form>
-			</section>
 
-			<section id="faqQuestions">
-				<?php 	
-					$questions_array = [
-						[
-							"title" => "How is NHL Stenden handeling the situation around Covid-19 virus?",
-							"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-						],
-						[
-							"title" => "Where can I order my books?",
-							"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-						],
-						[
-							"title" => "How can I see my shedule?",
-							"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-						],
-						[
-							"title" => "How can I contact my teacher?",
-							"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-						],
-						[
-							"title" => "What do I need to do when I'm sick?",
-							"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-						]
+ <div id="faqMain">
+	<section id="faqUserSearchBox">	        
+		<form>
+			<button type="submit" class="searchBarButton"><li class="fa fa-search iconSearch"></li></button>
+        	<input type="text" name="search" class="searchBar" placeholder="Search...">
+        	
+        </form>
+	</section>
+	<section id="faqQuestions">
+		<?php 	
+			$questions_array = [
+				[
+					"title" => "How is NHL Stenden handeling the situation around Covid-19 virus?",
+					"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				],
+				[
+					"title" => "Where can I order my books?",
+					"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				],
+				[
+					"title" => "How can I see my shedule?",
+					"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				],
+				[
+					"title" => "How can I contact my teacher?",
+					"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				],
+				[
+					"title" => "What do I need to do when I'm sick?",
+					"description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				]
 
-					];
+			];
 
-					// Displays the questions
-					foreach ($questions_array as $key => $value) {
-						echo "<details>
-									<summary>$value[title]</summary>
-									<p>$value[description]</p>
-								</details>";
-					}
-				?>
-			</section>
-    	</main>
-    	<footer></footer>
-    </body>
-</html>
+			$page = "FAQ";
+
+			// Displays the questions
+			foreach ($questions_array as $key => $value) {
+				echo "<details>
+							<summary>$value[title]</summary>
+							<p>$value[description]</p>
+						</details>";
+			}
+		?>
+	</section>
+</div>
