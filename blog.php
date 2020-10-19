49eaba3ef6c1eb6cab523cc91a314c48659ba2f7
jeffrey0402 <jr.roossien@gmail.com>
2020-10-19 10:08:29 +0200

    Modified PHP code, added styling
    Modified PHP code, added styling

--- blog.php
+++ blog.php
@@ -5,51 +5,45 @@
         <title>Blog</title>
         <link rel="stylesheet" href="style.css">
     </head>
-    <body>
-        <h1>Blog</h1>
+    <body>        
+        <?php
+        //Feed URLs (selector hier?)
+        $feeds = array(
+            "https://www.nu.nl/rss/Tech",
+            "blog.xml",
+            "https://www.nu.nl/rss/Algemeen"
+        );
         
-      		<?php
-      		//Code die ik zeker zelf gemaakt heb!
-/*				$xml=("https://www.nu.nl/rss/Tech");*/
-				$xml=("blog.xml");
-				$xmlDoc = new DOMDocument();
-				$xmlDoc->load($xml);
-				//get elements from "<channel>"
-				$channel=$xmlDoc->getElementsByTagName('channel')->item(0);
-				$channel_title = $channel->getElementsByTagName('title')
-				->item(0)->childNodes->item(0)->nodeValue;
-				$channel_link = $channel->getElementsByTagName('link')
-				->item(0)->childNodes->item(0)->nodeValue;
-				$channel_desc = $channel->getElementsByTagName('description')
-				->item(0)->childNodes->item(0)->nodeValue;
-				//output elements from "<channel>"
-				echo("<p><a href='" . $channel_link
-				  . "'>" . $channel_title . "</a>");
-				echo("<br>");
-				echo($channel_desc . "</p>");
-				//get and output "<item>" elements
-				$x=$xmlDoc->getElementsByTagName('item');
-				echo '<div id="flexcontainer">';
-				for ($i=0; $i<12; $i++) {
-					echo "<div>";
-					$item_title=$x->item($i)->getElementsByTagName('title')
-					->item(0)->childNodes->item(0)->nodeValue;
-					$item_category=$x->item($i)->getElementsByTagName('category')
-					->item(0)->childNodes->item(0)->nodeValue;
-					$item_link=$x->item($i)->getElementsByTagName('link')
-					->item(0)->childNodes->item(0)->nodeValue;
-					$item_desc=$x->item($i)->getElementsByTagName('description')
-					->item(0)->childNodes->item(0)->nodeValue;
-					echo ("<p><a href='" . $item_link
-					. "'>" . $item_title . "</a>");
-					echo ("<br>");
-					echo ($item_desc . "</p>");
-					echo ("<br>");
-					echo ($item_category . "</p>");
-					echo "</div>";
-				}
-				echo "</div>"
-			?>
-		</div>
+        //Read each feed's items
+        $entries = array();
+        foreach($feeds as $feed) {
+            $xml = simplexml_load_file($feed);
+            $entries = array_merge($entries, $xml->xpath("//item"));
+        }
+        
+        //Sort feed entries by pubDate
+        usort($entries, function ($feed1, $feed2) {
+            return strtotime($feed2->pubDate) - strtotime($feed1->pubDate);
+        });
+        
+        ?>
+        
+        <div class="flexcontainer">
+            <?php
+            //Print all the entries
+            foreach($entries as $entry){
+                ?>
+                <div>
+                	<div>
+	                    <h3><?= $entry->title ?></h3>
+	                    <p><?= strftime('%A %e %B %Y %T', strtotime($entry->pubDate)) ?></p>
+	                    <p><?= $entry->description ?></p>
+	                    <a class="leesmeer" href="<?= $entry->link ?>">Lees Meer&gt;&gt;</a>
+                	</div>
+            	</div>
+            <?php
+        }
+        ?>
+        </ul>
     </body>
 </html>
