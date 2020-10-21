bce0b706fef5ee097d8c944b6ccf7bc9079ee4cc
jeffrey0402 <jr.roossien@gmail.com>
2020-10-21 21:31:50 +0200

    Feed selector added, small flexbox layout changes
    Changed layout of the content, added feed selector, added search bar (not yet working), changed the layout of the flexboxes: added categories, added padding.

--- blog.php
+++ blog.php
@@ -5,15 +5,42 @@
         <title>Blog</title>
         <link rel="stylesheet" href="style.css">
     </head>
-    <body>        
+    <body>
+        <!-- Check what feed is selected, and if none is set, load "everything" -->
         <?php
-        //Feed URLs (selector hier?)
-        $feeds = array(
+            if(isset($_POST["submitblogfeed"])) {
+                $selectedfeed = $_POST['blogselector'];
+            }
+        ?>        
+        <?php
+        //default feed
+            if (empty($selectedfeed)) {
+            $selectedfeed = "everything";
+       }
+
+        if ( $selectedfeed == "everything" ) {
+            $feeds = array(
             "https://www.nu.nl/rss/Tech",
             "blog.xml",
-            "https://www.nu.nl/rss/Algemeen"
+            "http://feeds.feedburner.com/tweakers/nieuws"
         );
-        
+        //rest of the feeds, replace with proper xml files later
+        }
+        else if ($selectedfeed == "tweakers") {
+            $feeds = array(
+            "http://feeds.feedburner.com/tweakers/nieuws"
+        );
+        }
+        else if ($selectedfeed == "nu") {
+            $feeds = array(
+            "https://www.nu.nl/rss/Tech"
+        );
+        }
+        else if ($selectedfeed == "blog") {
+            $feeds = array(
+            "blog.xml"
+        );
+        }
         //Read each feed's items
         $entries = array();
         foreach($feeds as $feed) {
@@ -27,23 +54,54 @@
         });
         
         ?>
-        
-        <div class="flexcontainer">
+        <div class="blogcontent">
+            <div class="selectorcontainer">
+            	<p>Selecteer feed:</p>
+            	<form action="blog.php" method="post"> 
+    	        	<select name="blogselector">
+    	        		<option value="everything">Everything</option>
+                       <!--  not yet implemented options
+    	        		<option value="news">News</option>
+    	        		<option value="important">Important</option>
+                        <option value="entertaining">Entertaining</option>
+                        Temp rss feed links -->
+    	        		<option value="nu">nu.nl</option>
+    	        		<option value="blog">blog.xml</option>
+    	        		<option value="tweakers">Tweakers</option>
+            		</select>
+            		<input type="submit" name="submitblogfeed" value="Submit"/>
+            	</form>
+            </div>
+            <!-- Searchbox code geleend van de FAQ code -->
+            <div class="searchcontainer">
+                <section id="faqUserSearchBox">         
+                <form>
+                    <button type="submit" class="searchBarButton">
+                        <li class="fa fa-search iconSearch"></li>
+                    </button>
+                    <input type="text" name="search" class="searchBar" placeholder="Search...">
+                </form>
+                </section>
+            </div>        	
+        </div>
+        <!-- Flexbox met de RSS reader content -->
+        <div class="blogcontentflex">
             <?php
             //Print all the entries
             foreach($entries as $entry){
                 ?>
                 <div>
                 	<div>
+                        <p class="blogcategorie"><?= $entry->category?></p> <!-- This should have a background later, to improve readability and attract more readers, if/else structure with colors -->
 	                    <h3><?= $entry->title ?></h3>
 	                    <p><?= strftime('%A %e %B %Y %T', strtotime($entry->pubDate)) ?></p>
 	                    <p><?= $entry->description ?></p>
-	                    <a class="leesmeer" href="<?= $entry->link ?>">Lees Meer&gt;&gt;</a>
+	                    <a class="leesmeer" href="<?= $entry->link ?>">Lees Meer</a>
                 	</div>
             	</div>
-            <?php
-        }
-        ?>
+            <?php  }
+        	?>
+      
         </ul>
     </body>
 </html>
