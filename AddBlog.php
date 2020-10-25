76f47f8c8d0eccd2292e781cc12cb8fd2d25f02d
TBN2 <lucashaytink@gmail.com>
2020-10-25 16:34:22 +0100

    De twee individuele pagina's samengevoegd
    Met deze commit zijn de twee aparte invoerpagina's een geworden, om hiermee het probleem met het opslaan naar xml op te lossen

--- AddBlog.php
+++ AddBlog.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <?php
+        if (isset($_POST['submitpost'])) {
+            $xml = simplexml_load_file('Article.xml');
+            $post = $xml->addChild('item', '');
+            $post->addChild('category', $_REQUEST["CategoryAB"]);
+            $post->addChild('title', $_REQUEST["titleAB"]);
+            $post->addChild('description', $_REQUEST["descriptionAB"]);
+            $post->addChild('content', $_REQUEST["ContentAB"]);
+            print_r($xml);
+            echo $xml->saveXML("Article.xml");     
+			header("Location: blog.php");
+		}
 
-if(isset($_POST['submit1']))
+if(isset($_POST['submitpost']))
 
 	if($_FILES['Image'] !== NULL)
 	{
@@ -13,12 +24,11 @@ if(isset($_POST['submit1']))
 					$tmp_name = $_FILES["Image"]["tmp_name"];
 					$name = $_POST["TitleAB"].'_'.basename($_FILES["Image"]["name"]);
 					move_uploaded_file($tmp_name, "$uploads_dir/$name");
+					
 				}
 				else
 				{
 					echo "error, verkeerd type bestand toegevoegd";
-					$_FILES['Image'] = NULL;
-					header("AddBlog.php");
 				}
 	}
 
@@ -31,25 +41,32 @@ if(isset($_POST['submit1']))
 		<title>Add new post | page 1</title>
     </head>
     <body>
-    	<main class="addblog">
-	        <form id="mylittleformy" action="AddBlogPost.php" method="post" enctype="multipart/form-data">
-	        	<select name="CategoryAB" required id="ABcategory">
-	        		<option value="" disabled selected>Select Category *</option>
-	        		<option value="News">News</option>
-	        		<option value="Corona">Corona</option>
-	        		<option value="Events">Evenementen</option>
-	        	</select>
-	        	<label class="l1" for="NameArticle">Title: *</label>
-	        	<input type="text" required id="NameArticle" placeholder="Input Title" name="titleAB" autocomplete="off"><br>
-	        	<label class="l2" for= "AddBlog">Description *</label>
-	        	<textarea class="AddBlog" required placeholder="Input text..." name="descriptionAB"></textarea>
-				<label class="l3" for="Image">insert image here</label>
-				<input type="file" name="Image" action="AddBlog.php">
-	        	<div class="buttonsAB">
-	        		<button class="whiteAB" type="reset">Reset</button>
-	        		<button class="blueAB" type="submit" name="submit1">Next</button>
-	        	</div>
+		<div class="addblog">
+			<form id="mylittleformy" action="AddBlog.php" method="post" enctype="multipart/form-data">
+				<div class="addblog">
+					<select name="CategoryAB" required id="ABcategory">
+						<option value="" disabled selected>Select Category *</option>
+						<option value="News">News</option>
+						<option value="Corona">Corona</option>
+						<option value="Events">Evenementen</option>
+					</select>
+					<label for="NameArticle">Title: *</label>
+					<input type="text" required id="NameArticle" placeholder="Input Title" name="titleAB" autocomplete="off"><br>
+					<label for= "AddBlog">Description *</label>
+					<textarea class="AddBlog" required placeholder="Input text..." name="descriptionAB"></textarea>
+					<label for="Image">insert image here</label>
+					<input type="file" name="Image" action="AddBlog.php">
+
+					<div id="ArtikelContent">
+						<h1 class="h1AB">Content Article</h1>
+						<textarea name="ContentAB" required id="txtAC" placeholder="Please enter the content of your article here..."></textarea>
+						<div class="buttonsAB">
+							<button class="whiteAB" type="reset">Reset</button>
+							<button class="blueAB" type="submit" name="submitpost"><u>Submit</u></button>
+						</div>
+					</div>
+				</div>
 	    	</form>
-		</main>
+		</div>
     </body>
 </html>
