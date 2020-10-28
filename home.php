f1ad3417c4cce4a98bdf09486118bcd30341fbf0
KillerSlang <72264057+KillerSlang@users.noreply.github.com>
2020-10-28 11:48:57 +0100

    Login toegevoegd

--- home.php
+++ home.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+if($_SESSION['login'] != true)
+{
+
+}
+else{
+            echo <<<HTML
+        <a href="errorpage.php"> </a> 
+        HTML;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
