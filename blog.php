66b023ee03baf77d5deb82713b7226a860acd5e1
jeffrey0402 <jr.roossien@gmail.com>
2020-10-28 11:26:09 +0100

    changed template.php with index.php
    changed template.php with index.php

--- blog.php
+++ blog.php
@@ -42,7 +42,7 @@
             <div class="selectorcontainer">
                 <!-- Category selector -->
             	<p>Select category:</p>
-            	<form action="template.php?Blog" method="post"> 
+            	<form action="index.php?Blog" method="post"> 
     	        	<select class="blogselect" name="blogselector">
     	        		<option value="everything">Everything</option>
     	        		<option value="News">News</option>
@@ -54,7 +54,7 @@
             	</form>
             </div>
             <div class="add-blog-button">
-                <a class="add-blog-button" href="template.php?AddBlog">   
+                <a class="add-blog-button" href="index.php?AddBlog">   
                     <p><i class="fas fa-plus-circle"></i>Add Post</p>                                     
                 </a>
             </div>
@@ -64,7 +64,7 @@
 		            <input type="text" id="search" placeholder="Search..." class="blogsearchbarbutton">
 		            <input class="searchButton" type="button" name="search" value="Go" onclick="search(document.getElementById('search').value)">
                    <!--  Reset button -->
-                    <input class="searchButton" type="button" name="reset" value="Reset" onclick="document.location.href='template.php?Blog'">
+                    <input class="searchButton" type="button" name="reset" value="Reset" onclick="document.location.href='index.php?Blog'">
         		</div>
             </div>        	
         </div>
