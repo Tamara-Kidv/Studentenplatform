6edbfafd86a0ea5ce6d36177ea2f8606841a42d4
Tamara <tamarakijkindevegte@live.nl>
2020-10-28 12:36:47 +0100

    Lasts changes
    Fixed some small issues with redirect

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <?php
+            session_start();
+
+            if($_SESSION['login'] = true) {
+                header('Location: inlog.php');
+                exit;
+            } 
+        ?>
         <meta charset="UTF-8">
         <title>Home</title>
         <link rel="stylesheet" href="style.css">
@@ -10,19 +18,23 @@
     <body>
         <header>
             <img id="logo" src="images/logo.png" alt="logo">
-                <a id="navhome" href="index.php?Home">Home</a>
-                <a id="navblog" href="index.php?Blog">Blog</a>
+                <a id="navhome" href="?Home">Home</a>
+                <a id="navblog" href="?Blog">Blog</a>
                 <a id="navfaq" href="?FAQ">FAQ</a>
-                <a id="navcontact" href="index.php?Contact">Contact</a> 
+                <a id="navcontact" href="?Contact">Contact</a> 
                 <div id="placelang">
                     <select id="lang" name="language">
                         <option value="eng">ENG</option>
                         <option value="nl">NL</option>
                     </select>
+                    <div><a href="?Loguit">Log uit</a></div>
                 </div>
         </header>
         <div id="banner">
             <?php
+                // if (isset($_GET['Loguit'])) {
+                //     die;
+                // }
                 if(isset($_GET['Home'])) {
                     $page = "Home";
                 } else if(isset($_GET['Blog'])) {
@@ -48,7 +60,7 @@
         </main>
         <footer>       
             <p id="footerdate">	&copy; 2020 - 2021</p>
-            <p id="footerprivacy">privacystatement - <a href="profile.php">profile</a></p>
+            <p id="footerprivacy">privacystatement - <a href="?Profile">profile</a></p>
         </footer>
     </body>
 </html>
