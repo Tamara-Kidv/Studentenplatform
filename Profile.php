d41195f4b748c441e447f88b8ba6afb589a9b5ee
JulianVeringa <72262082+JulianVeringa@users.noreply.github.com>
2020-10-28 12:15:05 +0100

    Create Profile.php

---
+++ Profile.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profile pagina</title>
+    </head>
+    <body>
+        
+        <?php
+$email= "julian.veringa@student.nhlstenden.com";
+$een = strpos($email, '@');
+$naam1 = substr($email, 0, $een);
+
+$bak2 = strpos($naam1, '.');
+$naam2 = substr($naam1, 0, $bak2);
+
+$bietn3 = ($bak2 + 1);
+$naam3 = substr($naam1, $bietn3);
+?>
+        
+        <p> Naam: <?php echo $naam2. " " .$naam3; ?> </p>
+        <P> Email: <?php echo $email ?> </p>
+    </body>
+</html>
